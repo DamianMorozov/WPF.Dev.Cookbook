0eade51876add5da8e70e3a8dd102104d310564d
Morozov Damian <damian@morozov33.ru>
2020-02-25 15:03:40 +0300

    Updated CH02.CalendarDemo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [0.2.37] - 2020-02-25
+### Updated
+  [*] CH02.CalendarDemo
+
 ## [0.2.36] - 2020-02-21
 ### Updated
   [*] CH02.SliderDemo
--- Chapter02/CH02.CalendarDemo/MainWindow.xaml.cs
+++ Chapter02/CH02.CalendarDemo/MainWindow.xaml.cs
@@ -1,35 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CH02.CalendarDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void OnSelectedDateChanged(object sender, 
-                                        SelectionChangedEventArgs e)
+        private void OnSelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             MessageBox.Show("You selected: " + 
-                ((DateTime)e.AddedItems[0]).ToString("dd-MMM-yyyy"));
+                ((DateTime)e.AddedItems[0]).ToString("yyyy-MM-dd"));
         }
     }
 }
