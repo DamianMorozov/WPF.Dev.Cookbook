8071c367d9a1f817fce42c726b59431ba373b546
Morozov Damian <damian@morozov33.ru>
2020-03-02 14:00:16 +0300

    Updated CH03.GridDemo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [0.2.42] - 2020-03-02
+### Updated
+  [*] CH03.GridDemo
+
 ## [0.2.41] - 2020-02-27
 ### Updated
   [*] CH02.StatusBarDemo
--- Chapter03/CH03.GridDemo/App.xaml.cs
+++ Chapter03/CH03.GridDemo/App.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace CH03.GridDemo
+﻿namespace CH03.GridDemo
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
